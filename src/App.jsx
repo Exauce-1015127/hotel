561297a5d6668f8446f1402825af75c370688ace
Exauce-1015127 <1015127@hr.nl>
2026-02-26 01:12:39 +0100

    - Created navbar component - Implemented BrowserRouter to use page routing - Added content inside favorites page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import Home from "./pages/Home"
+import {Route, Routes} from "react-router-dom";
+import Favorites from "./pages/Favorites.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
 
   return (
-    <>
-      <h1>Vite + React</h1>
-        <Home />
-    </>
+      <div>
+          <NavBar/>
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                </Routes>
+            </main>
+      </div>
   )
 }
 
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites() {
+    return <div className="favorites-empty">
+        <h1>Favorites</h1>
+        <p>Je hebt nog geen favorieten</p>
+    </div>
+}
+
+export default Favorites
