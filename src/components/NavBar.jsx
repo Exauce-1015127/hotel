f4835c85b19d23cb8bdac14311afa692ff66a57f
Exauce-1015127 <1015127@hr.nl>
2026-02-27 22:36:47 +0100

    - Changed colors on homepage - Added animation to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,11 +5,11 @@ import "../css/Navbar.css"
 function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/" className="nav-title">MOVIE FANATICS</Link>
         </div>
         <div className="navbar-links">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/favorites" className="nav-link">Favorites</Link>
+            <Link to="/" className="nav-link">HOME</Link>
+            <Link to="/favorites" className="nav-link">FAVORITES</Link>
         </div>
     </nav>
 }
